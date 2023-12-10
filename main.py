ac80f1275fdc2d95bb719832b0ee3c4d4aaf7fe4
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-12-11 01:35:12 +0530

    core

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from fastapi import FastAPI, Depends
+import uvicorn as uv
+
+from model import EmailDetectionModel, PredictionOutput
+
+__version__ = "0.1.0"
+
+app = FastAPI(
+    title="ML Endpoint 🤸🏻‍♀️",
+    description="""<h2>ML Endpoint using FastAPI and ScikitLearn<h2>""",
+    version=__version__,
+)
+model = EmailDetectionModel()
+
+
+@app.get("/")
+async def root() -> dict:
+    return {"message": "Hello 👋"}
+
+
+@app.post("/api/v1/prediction")
+async def prediction(
+    output: PredictionOutput = Depends(model.predict),
+) -> PredictionOutput:
+    return output
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    model.load_model()
+
+
+if __name__ == "__main__":
+    uv.run("main:app", reload=True, port=8000)
