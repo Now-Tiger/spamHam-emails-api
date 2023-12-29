7f2acdf78b9dc4452a33c018a5b1d6a351474f98
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-12-30 03:13:20 +0530

    updated structure for scattered files

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 from fastapi import FastAPI, Depends
 import uvicorn as uv
 
+sys.path.insert(1, "./models/")
 from model import EmailDetectionModel, PredictionOutput
 
+
 __version__ = "0.1.0"
+
 
 app = FastAPI(
     title="ML Endpoint 🤸🏻‍♀️",
     description="""<h2>ML Endpoint using FastAPI and ScikitLearn<h2>""",
     version=__version__,
 )
+
+
 model = EmailDetectionModel()
 
 
--- models/model.py
+++ models/model.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 import os
 import joblib
-from typing import Optional
+from typing import Optional, List
 
 from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 from sklearn.pipeline import Pipeline
 
 from warnings import filterwarnings
-filterwarnings('always')
-filterwarnings('ignore')
+
+filterwarnings("always")
+filterwarnings("ignore")
+
+model_file = os.path.join(
+    os.path.dirname(__file__), "email_phishing_detection.0.1.0.joblib"
+)
+
+loaded_model: tuple[Pipeline, list[str]] = joblib.load(model_file)
+model, targets = loaded_model
+vectorizer_file = os.path.join(os.path.dirname(__file__), "./vectorizer.joblib")
+vectorizer = joblib.load(vectorizer_file)
 
 
 class PredictionInput(BaseModel):
@@ -22,30 +32,28 @@ class PredictionInput(BaseModel):
 
 class PredictionOutput(BaseModel):
     label: int
-    
+
 
 class EmailDetectionModel:
-    """ 
+    """
     Loads model and Makes prediction
     """
+
     model: Optional[Pipeline]
     targets: Optional[list[str]]
 
     def load_model(self) -> None:
-        """ Load the model """
-        model_file = os.path.join(os.path.dirname(__file__), 'email_phishing_detection.0.1.0.joblib')
-        loaded_model: tuple[Pipeline, list[str]] = joblib.load(model_file)
-        model, targets = loaded_model
+        """Load the model"""
         self._model = model
         self._targets = targets
-        self._vectorizer = joblib.load('vectorizer.joblib')
+        self._vectorizer = vectorizer
 
     async def predict(self, input: PredictionInput) -> PredictionOutput:
-        """ Runs a prediction on given input string.
-            * if input is empty raise an HTTP 404 error
-            * else run prediction
+        """Runs a prediction on given input string.
+        * if input is empty raise an HTTP 404 error
+        * else run prediction
         """
-        if input.text == None or len(input.text) < 1:
+        if not input.text or len(input.text) < 1:
             raise HTTPException(status_code=404, detail="Empty input string provided.")
         else:
             transformed_text = self._vectorizer.transform([input.text])
